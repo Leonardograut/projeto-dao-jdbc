8459a878a3a78f41fef02ba18561cca03ed6df30
Leonardo Nascimento <omgleo@live.com>
2023-03-26 17:48:06 -0300

    Departamento entidade class

---
+++ src/principal/Program.java
@@ -0,0 +1,15 @@
+package principal;
+
+import model.entidades.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1,"Livros");
+		
+		System.out.println(obj);
+
+	}
+
+}
